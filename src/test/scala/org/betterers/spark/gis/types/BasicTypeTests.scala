d20aa70bf8604ca806711e89dd35aab9c3d0757e
Emiliano Leporati <emiliano.leporati@gmail.com>
2015-05-12 01:55:22 -0400

    fixed directory names

--- src/test/scala/org/betterers/spark/gis/types/BasicTypeTests.scala
+++ src/test/scala/org/betterers/spark/gis/types/BasicTypeTests.scala
@@ -1,31 +1,62 @@
-package scala.org.betterers.spark.gis.types
+package org.betterers.spark.gis.types
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.test.TestSQLContext._
 import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import org.apache.spark.sql.{Row, SQLContext}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.betteres.spark.gis.types.GeometryType
 import org.scalatest.FunSuite
-
-import org.betteres.spark.gis.types.Geometry
 
 /**
  * Basic tests for GIS UDTs
  */
-class BasicTypeTests extends FunSuite{
+class BasicTypeTests extends FunSuite {
   val sqlContext = new SQLContext(sparkContext)
+  val schema = StructType(Seq(
+    StructField("id", IntegerType),
+    StructField("geo", GeometryType.T)
+  ))
+  val jsons = Map(
+    (1,"{\"type\":\"Point\",\"coordinates\":[1,1]}}"),
+    (2,"{\"type\":\"LineString\",\"coordinates\":[[12,13],[15,20]]}}")
+  )
 
-  test("How are they serialized") {
-    val schema = StructType(Seq(StructField("id", IntegerType), StructField("geo", Geometry.Type)))
-    val rdd = sparkContext.parallelize(Seq(
-      Row(1, "{\"type\":\"Point\",\"coordinates\":[1,1]}}"),
-      Row(2, "{\"type\":\"Point\",\"coordinates\":[12,13]}}")
-    ))
-    val schemaRDD = sqlContext.createDataFrame(rdd, schema)
-    schemaRDD.foreach(println)
+  test("Serialize") {
+    val point = new GeometryType((1.0, 1.0))
+    println(point.toJson)
+    println(point.toGeoJson)
+    val line = new GeometryType((1.0, 1.0), (2.0, 2.0))
+    println(line.toJson)
+    println(line.toGeoJson)
+    val poly = new GeometryType((1.0, 1.0), (2.0, 2.0), (3.0, 3.0), (4.0, 4.0))
+    println(poly.toJson)
+    println(poly.toGeoJson)
   }
 
-  test("Point types from GeoJSON") {
+  test("Deserialize") {
+    val data = Seq(
+      Row(1, GeometryType.fromGeoJson(jsons(1))),
+      Row(2, GeometryType.fromGeoJson(jsons(2)))
+    )
+    println(data)
+  }
+
+  test("In RDDs") {
+    val data = Seq(
+      Row(1, GeometryType.fromGeoJson(jsons(1))),
+      Row(2, GeometryType.fromGeoJson(jsons(2)))
+    )
+    val rdd = sparkContext.parallelize(data)
+    val df = sqlContext.createDataFrame(rdd, schema)
+    df.foreach(println)
+  }
+
+  test("Load JSON RDD with explicit schema") {
+    val rdd = sparkContext.parallelize(Seq(
+      "{\"id\":1,\"geo\":" + jsons(1) + "}",
+      "{\"id\":2,\"geo\":" + jsons(2) + "}"
+    ))
+    val df = sqlContext.jsonRDD(rdd, schema)
+    df.foreach(println)
   }
 }
 
